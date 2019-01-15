27c554e008d1dec7156835e88e705016e7f5815e
ali-mohammed200 <ali.mohammed200@gmail.com>
2019-01-15 10:04:37 -0500

    Done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,14 @@
 class BoatingTest
-
+  attr_reader :student, :testName, :testStatus, :instructor
+  @@all = []
+  def initialize(student, testName, testStatus, instructor)
+    @student = student
+    @testName = testName
+    @testStatus = testStatus
+    @instructor = instructor
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
 end
