46b243b06d9f1e5465c09f8028c54781b6fb5e8b
github username <github email address>
2019-01-15 08:52:13 -0500

    done

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
 
+  attr_reader :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def tests
+    BoatingTest.all.select {|test| test.student == self}
+  end
+
+  def add_boating_test(test_name, test_status, instructor)
+    BoatingTest.new(self, test_name, test_status, instructor)
+  end
+
+  def grade_percentage
+    total = self.tests.count.to_f
+    passed = self.tests.select {|test| test.test_status == "Passed"}.count.to_f
+    percentage = passed / total * 100
+  end
+
+  def self.find_student(name)
+    Student.all.select {|stud| stud.name == name}
+  end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -4,10 +4,21 @@ def reload
   load 'config/environment.rb'
 end
 
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+stud1 = Student.new("Cassidy")
+stud2 = Student.new("Shivani")
+stud3 = Student.new("Liz")
+
+inst1 = Instructor.new("Prince")
+inst2 = Instructor.new("Helen")
+inst3 = Instructor.new("Mar")
+
+test1 = BoatingTest.new(stud1, "Level 1", "Incomplete", inst1)
+test2 = BoatingTest.new(stud2, "Level 2", "Passed", inst2)
+test3 = BoatingTest.new(stud2, "Level 3", "Incomplete", inst3)
+test4 = BoatingTest.new(stud1, "Level 1", "Passed", inst1)
+test4 = BoatingTest.new(stud1, "Level 3", "Passed", inst3)
+
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
-
