d0b3c0617dc927a0548e7266689affa8d5ce7c77
Your Name <you@example.com>
2019-01-15 00:29:30 -0500

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,32 @@
 class Instructor
+  attr_accessor :name, :student_name, :test_name, :status
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def pass_student(student_name, test_name)
+    pass_array = BoatingTest.all.select do |test|
+      test.student == student_name
+    end
+    pass_array.map do |test|
+      test.test_status = 'passed'
+    end
+  end
+
+  def fail_student(student_name, test_name)
+    fail_array = BoatingTest.all.select do |test|
+      test.student == student.name
+    end
+    fail_array / BoatingTest.all.size
+    # fail_array.map do |test|
+    #   test.test_status = 'failed'
+  end
 
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,3 +1,34 @@
 class Student
+  attr_reader :first_namem :last_name
+  @@all = []
+
+  def initialize(first_name)
+    @first_name = first_name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_boating_test(test_name, status, instructor)
+    BoatingTest.new(self, test_name, status, instructor)
+  end
+
+  def self.find_student(first_name)
+    Student.all.select do |student|
+      student.first_name == self
+    end
+  end
+
+  def grade_percentage
+    passed_arrray = BoatingTest.all.select do |test|
+      test.test_status == 'passed'
+    end
+    failed_array = BoatingTest.all.select do |test|
+      test.test_status == 'failed'
+    end
+    total_array = 
+  end
 
 end
