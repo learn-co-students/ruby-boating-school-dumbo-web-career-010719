34ba3f38f4e182c706843ad51e9e4eb01d76de13
Tony_Huang <tong@Tony-Huangs-MacBook-Pro.local>
2019-01-14 19:19:04 -0500

    done

--- app/models/boatingtest.rb
+++ app/models/boatingtest.rb
@@ -1,3 +1,26 @@
 class BoatingTest
 
+    attr_accessor :test_name, :test_status
+
+    @@all=[]
+
+    def initialize(student, test_name, test_status, instructor)
+        @student=student
+        @test_name=test_name
+        @test_status=test_status
+        @instructor=instructor
+        @@all<<self
+    end
+    
+    def self.all
+        @@all
+    end
+
+    def student
+        @student
+    end
+
+    def instructor
+        @instructor
+    end
 end
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,37 @@
 class Instructor
+    @@all=[]
+    def initialize(name)
+        @name=name
+        @@all<<self
+    end
 
+    def name
+        @name
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def pass_student(name, test_name)
+         BoatingTest.all.each do |boatingTest|
+             if boatingTest.student.first_name==name
+                boatingTest.test_status="passed"
+                return boatingTest
+            else
+              return  BoatingTest.new(Student.new(name), test_name, "passed", self)
+             end
+          end
+    end
+
+    def fail_student(name, test_name)
+         BoatingTest.all.each do |boatingTest|
+             if boatingTest.student.first_name==name
+                boatingTest.test_status="failed"
+                return boatingTest
+            else
+                 return BoatingTest.new(Student.new(name), test_name, "failed", self)
+             end
+        end
+    end
 end
